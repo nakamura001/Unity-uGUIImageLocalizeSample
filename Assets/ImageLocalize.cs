d5870dd75198a6194db4ce6516f435413a4d4417
nakamura001 <polluxtk@gmail.com>
2014-12-19 19:48:31 +0900

    全ソースコードをアップ。

---
+++ Assets/ImageLocalize.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Globalization;
+using System;
+
+public class ImageLocalize : MonoBehaviour {
+	void Awake () {
+		Image img = gameObject.GetComponent<Image>();
+		if (img) {
+			string language = Application.systemLanguage.ToString();
+			string texturePath = "images/" + language + "/" + img.sprite.name;
+			Texture2D tex = (Texture2D)Instantiate(Resources.Load(texturePath));
+			RectTransform rt = gameObject.GetComponent<RectTransform>();
+			Sprite sp = img.sprite;
+			Sprite newSp = Sprite.Create(tex, sp.rect, rt.pivot, sp.pixelsPerUnit);
+			img.sprite = newSp;
+		}
+	}
+}
